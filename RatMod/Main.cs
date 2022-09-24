2b46dfacf44902488201c29b0cc49ac24d01a164
PetersonE1 <PetersonE_1@live.com>
2022-09-24 15:31:26 -0400

    Creating base project files

---
+++ RatMod/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UMM;
+
+namespace RatMod
+{
+    [UKPlugin("Rat With A Gun", "1.0.0", "Adds a collection of rats with a lust for blood", false, true)]
+    public class Main
+    {
+    }
+}
