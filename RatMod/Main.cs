8a42be14651c428c53e8cf2082cc0564d66756bd
PetersonE1 <PetersonE_1@live.com>
2023-02-07 22:05:24 -0500

    greech knife

--- RatMod/Main.cs
+++ RatMod/Main.cs
@@ -12,16 +12,17 @@ using UnityEngine.SceneManagement;
 namespace RatMod
 {
     [UKDependency("petersone1.ultrakitreloaded", "1.3.2")]
-    [UKPlugin("petersone1.ratwithagun", "Rat With A Gun", "0.2.0", "Adds a collection of rats with a lust for blood", false, true)]
+    [UKPlugin("petersone1.ratwithagun", "Rat With A Gun", "0.2.1", "Adds a collection of rats with a lust for blood", false, true)]
     public class Main : UKMod
     {
-        private AssetBundle bundle = AssetBundle.LoadFromFile(Directory.GetCurrentDirectory() + @"\BepInEx\UMM Mods\RatWithAGun\petersone1_ratwithagun.assetBundle");
-        private Weapon[] weapons;
+        private AssetBundle bundle = AssetBundle.LoadFromMemory(Properties.Resources.petersone1_ratwithagun);
+        public Weapon[] weapons;
 
         public override void OnModLoaded()
         {
             SceneManager.sceneLoaded += OnSceneLoaded;
             weapons = ULTRAKIT.Loader.WeaponLoader.LoadWeapons(bundle);
+            ULTRAKIT.Loader.BuffLoader.RegisterBuff(Bigger.buff);
         }
 
         public override void OnModUnload()
