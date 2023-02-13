7a829a9d6ca654f507f0a33e3e061c2c152626cd
PetersonE1 <PetersonE_1@live.com>
2023-02-13 15:53:09 -0500

    greech knife complete (for now)

--- RatMod/Patches/KnifePatches.cs
+++ RatMod/Patches/KnifePatches.cs
@@ -7,18 +7,24 @@ using UnityEngine;
 using HarmonyLib;
 using RatMod.Weapon_Scripts.Object_Scripts;
 using ULTRAKIT.Extensions;
+using RatMod.Weapon_Scripts;
 
 namespace RatMod.Patches
 {
     [HarmonyPatch(typeof(EnemyIdentifier))]
     public static class EnemyIdentifierPatch
     {
+        private static readonly float HEAL_DISTANCE = 3f;
+        private static readonly float HEAL_AMOUNT = 1.5f;
         [HarmonyPatch("Death"), HarmonyPrefix]
         static void DeathPrefix(EnemyIdentifier __instance)
         {
             KnifeObject knife = __instance.GetComponentInChildren<KnifeObject>();
             if (knife)
                 knife.transform.parent = null;
+            knife = GreechKnife.ActiveKnife?.GetComponent<KnifeObject>();
+            if (knife && Vector3.Distance(__instance.transform.position, knife.transform.position) < HEAL_DISTANCE)
+                knife.timeLeft += HEAL_AMOUNT;
         }
     }
 }
