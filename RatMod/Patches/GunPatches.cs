94df561867c1e89e3533c698469a0c6648e388f5
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-03-30 11:36:34 -0400

    gun rat now refills ammo with blood, no longer has a reload

---
+++ RatMod/Patches/GunPatches.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RatMod.Weapon_Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ULTRAKIT.Extensions;
+using UnityEngine;
+
+namespace RatMod.Patches
+{
+    [HarmonyPatch(typeof(NewMovement))]
+    public static class NewMovementPatch
+    {
+        static readonly float HealthPerAmmo = 10f;
+        static float storedHealth = 0f;
+
+        [HarmonyPatch("GetHealth"), HarmonyPostfix]
+        static void GetHealthPostfix(NewMovement __instance, int health)
+        {
+            UKLogger.Log(health);
+            int ammoToAdd = 0;
+            storedHealth += health;
+            while (storedHealth >= HealthPerAmmo)
+            {
+                storedHealth -= HealthPerAmmo;
+                ammoToAdd++;
+            }
+            if (RatVariableManager.Instance)
+            {
+                RatVariableManager.Instance.GunRat_ammo = Mathf.Clamp(RatVariableManager.Instance.GunRat_ammo + ammoToAdd, 0, 7);
+                GunScript.Refresh();
+            }
+        }
+    }
+}
--- RatMod/Weapon Scripts/GunScript.cs
+++ RatMod/Weapon Scripts/GunScript.cs
@@ -11,10 +11,12 @@ namespace RatMod.Weapon_Scripts
 {
     public class GunScript : MonoBehaviour
     {
-        readonly float ReloadTime = 2.5f;
+        public static Action Refresh;
+
+        //readonly float ReloadTime = 2.5f;
         private bool cooldownOff;
         Transform origin;
-        MeshRenderer txt_reloading;
+        //MeshRenderer txt_reloading;
         MeshRenderer[] txt_num;
         GameObject beam;
         
@@ -22,12 +24,19 @@ namespace RatMod.Weapon_Scripts
         private InputActionState Fire1 = InputManager.Instance?.InputSource.Fire1;
         private InputActionState Fire2 = InputManager.Instance?.InputSource.Fire2;
 
+        private void RefreshCounter()
+        {
+            foreach (Renderer renderer in txt_num)
+                renderer.enabled = false;
+            txt_num[_man.GunRat_ammo].enabled = true;
+        }
+
         private void OnEnable()
         {
             InitializeFields();
-            if (_man.GunRat_reloading)
-                txt_reloading.enabled = true;
-            else
+            //if (_man.GunRat_reloading)
+            //    txt_reloading.enabled = true;
+            //else
                 txt_num[_man.GunRat_ammo].enabled = true;
             try
             {
@@ -52,7 +61,7 @@ namespace RatMod.Weapon_Scripts
 
             int ammo = _man.GunRat_ammo;
             bool reloading = _man.GunRat_reloading;
-            if (Fire1.WasPerformedThisFrame && ammo > 0 && !reloading)
+            if (Fire1.WasPerformedThisFrame && ammo > 0 /*&& !reloading*/)
             {
                 txt_num[ammo].enabled = false;
                 if (!cooldownOff)
@@ -64,35 +73,37 @@ namespace RatMod.Weapon_Scripts
                 shot.transform.position = origin.transform.position;
                 shot.transform.rotation = CameraController.Instance.transform.rotation;
 
-                if (ammo == 0)
+                /*if (ammo == 0)
                 {
                     reloading = true;
                     txt_num[ammo].enabled = false;
                     txt_reloading.enabled = true;
                     Invoke("Reload", ReloadTime);
-                }
+                }*/
             }
-            if (Fire2.WasPerformedThisFrame && ammo != 7 && !reloading)
+            /*if (Fire2.WasPerformedThisFrame && ammo != 7 && !reloading)
             {
                 reloading = true;
                 txt_num[ammo].enabled = false;
                 txt_reloading.enabled = true;
                 Invoke("Reload", ReloadTime);
-            }
+            }*/
             _man.GunRat_ammo = ammo;
-            _man.GunRat_reloading = reloading;
+            //_man.GunRat_reloading = reloading;
         }
 
         private void InitializeFields()
         {
+            Refresh = RefreshCounter;
+
             if (origin == null)
             {
                 origin = transform.Find("RAT/gun/MuzzleOrigin");
             }
-            if (txt_reloading == null) 
+            /*if (txt_reloading == null) 
             {
                 txt_reloading = transform.Find("RAT/gun/Screen/TEXT/Reloading").gameObject.GetComponent<MeshRenderer>();
-            }
+            }*/
             if (txt_num == null)
             {
                 txt_num = transform.Find("RAT/gun/Screen/TEXT/NUM").gameObject.GetComponentsInChildren<MeshRenderer>();
@@ -103,13 +114,13 @@ namespace RatMod.Weapon_Scripts
             }
         }
 
-        private void Reload()
+        /*private void Reload()
         {
             _man.GunRat_ammo = 7;
             _man.GunRat_reloading = false;
             txt_reloading.enabled = false;
             txt_num[_man.GunRat_ammo].enabled = true;
-        }
+        }*/
 
         private void OnCheatChange(string cheat)
         {
